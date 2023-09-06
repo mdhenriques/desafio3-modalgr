9c747058f8e491e1e929683e59efe8a41470f2f7
mdhenriques <md.henriques@outlook.com>
2023-09-06 15:20:39 -0300

    Commit para vercel

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,6 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginUserDTO } from 'src/user/dto/loginUser.dto';
-import { AuthGuard } from './auth.guard';
-import { User } from 'src/user/user.entity';
+import { LoginUserDTO } from '../user/dto/loginUser.dto';
 
 @Controller('auth')
 export class AuthController {
